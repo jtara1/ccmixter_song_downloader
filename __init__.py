9fe335ea06b22e11d4062cf4afb0d3ae9cb3e0a0
James T <jtara@tuta.io>
2017-11-06 21:06:35 -0800

    pytest working with setup and teardown

--- __init__.py
+++ __init__.py
@@ -1,10 +1,2 @@
-try:
-    from CCMixterSongDownloader.download_music import \
-        CCMixterSongDownloader
-    # from CCMixterSongDownloader.download_music import CCMixterSongDownloader
-except ModuleNotFoundError as e:
-    raise e
-    # from .CCMixterSongDownloader.download_music import CCMixterSongDownloader
-    # from download_music import CCMixterSongDownloader
-    # from .download_music import CCMixterSongDownloader
-    # from . import CCMixterSongDownloader
+from CCMixterSongDownloader.download_music import \
+    CCMixterSongDownloader
--- tests/test_CCMixterSongDownloader.py
+++ tests/test_CCMixterSongDownloader.py
@@ -1,13 +1,30 @@
-import pytest
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join("../", __file__)))
-from CCMixterSongDownloader import CCMixterSongDownloader
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from CCMixterSongDownloader.download_music import CCMixterSongDownloader
 
 
-def test_case1():
-    folder = os.path.join(__file__, 'tmp_dl')
-    dl = CCMixterSongDownloader()
-    dl.download(save_folder=folder, tags='classical', limit=2)
-    print(dl.download_info)
+downloads_folder = os.path.join(os.path.dirname(__file__), 'tmp_dl')
+
+
+def setup_function(function):
+    if not os.path.isdir(downloads_folder):
+        os.makedirs(downloads_folder)
+    return function
+
+
+def teardown_function(function):
+    function()
+    for file in os.listdir(downloads_folder):
+        os.remove(os.path.join(downloads_folder, file))
+    os.rmdir(downloads_folder)
     assert True
+
+
+@teardown_function
+@setup_function
+def test_case1():
+    dl = CCMixterSongDownloader()
+    dl.download(save_folder=downloads_folder, tags='classical', limit=1)
+    print(dl.download_info)
+    assert len(os.listdir(downloads_folder)) == 2  # .mp3 & history text file
